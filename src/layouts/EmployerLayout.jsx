ac0a03b6752210bafd2aa36994e207b70b316888
knh0301 <kimna6207@naver.com>
2025-11-16 18:32:25 +0900

    [Feat] 상단바 생성

--- src/layouts/EmployerLayout.jsx
+++ src/layouts/EmployerLayout.jsx
@@ -1,13 +1,18 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/layout/Header.jsx";
 import EmployerNav from "../components/layout/EmployerNav.jsx";
 
 export default function EmployerLayout() {
   return (
-    <div className="employer-layout">
-      <EmployerNav />
-      <main className="app-main-with-sidebar">
-        <Outlet />
-      </main>
-    </div>
+    <>
+      <Header />
+      <div className="employer-layout">
+        <EmployerNav />
+        <main className="app-main-with-sidebar">
+          <Outlet />
+        </main>
+      </div>
+    </>
   );
 }
+
--- src/layouts/WorkerLayout.jsx
+++ src/layouts/WorkerLayout.jsx
@@ -1,14 +1,17 @@
-
 import { Outlet } from "react-router-dom";
+import Header from "../components/layout/Header.jsx";
 import WorkerNav from "../components/layout/WorkerNav.jsx";
 
 export default function WorkerLayout() {
   return (
-    <div className="worker-layout">
-      <WorkerNav />
-      <main className="app-main-with-sidebar">
-        <Outlet />
-      </main>
-    </div>
+    <>
+      <Header />
+      <div className="worker-layout">
+        <WorkerNav />
+        <main className="app-main-with-sidebar">
+          <Outlet />
+        </main>
+      </div>
+    </>
   );
 }
