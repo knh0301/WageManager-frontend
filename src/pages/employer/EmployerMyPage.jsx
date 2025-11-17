73384ebed9e25fe1e26e0f2272cdae1eeab783f7
knh0301 <kimna6207@naver.com>
2025-11-17 16:25:55 +0900

    [Feat] [웹] employerMyPage 수정 기능 추가

--- src/pages/employer/EmployerMyPage.jsx
+++ src/pages/employer/EmployerMyPage.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 import "../../styles/employerMyPage.css";
 
 export default function EmployerMyPage() {
-  const user = {
+  const initialUser = {
     name: "김나현",
     birthDate: "2003-03-01",
     gender: "woman",
@@ -10,44 +12,129 @@ export default function EmployerMyPage() {
     password: "password123",
   };
 
+  const [user, setUser] = useState(initialUser);
+  const [editableSections, setEditableSections] = useState({
+    basic: false,
+    phone: false,
+    email: false,
+    password: false,
+  });
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleEdit = (section) => {
+    setEditableSections((prev) => ({
+      ...prev,
+      [section]: !prev[section],
+    }));
+  };
+
+  const handleChange = (field, value) => {
+    setUser((prev) => ({
+      ...prev,
+      [field]: value,
+    }));
+  };
+
   return (
     <div className="mypage-container">
       <h1 className="mypage-title">기본정보</h1>
       <div className="mypage-basic-info">
         <div className="mypage-name">
           <span className="mypage-label">이름</span>
-          <input type="text" value={user.name} />
+          <input
+            type="text"
+            value={user.name}
+            disabled={!editableSections.basic}
+            onChange={(e) => handleChange("name", e.target.value)}
+          />
         </div>
         <div className="mypage-brith">
           <span className="mypage-label">생년월일</span>
-          <input type="date" value={user.birthDate} />
+          <input
+            type="date"
+            value={user.birthDate}
+            disabled={!editableSections.basic}
+            onChange={(e) => handleChange("birthDate", e.target.value)}
+          />
         </div>
         <div className="mypage-gender">
           <span className="mypage-label">성별</span>
-          <select name="gender" id="" value={user.gender}>
+          <select
+            name="gender"
+            value={user.gender}
+            disabled={!editableSections.basic}
+            onChange={(e) => handleChange("gender", e.target.value)}
+          >
             <option value="man">남성</option>
             <option value="woman">여성</option>
           </select>
         </div>
-        <button className="mypage-edit-button">수정</button>
+        <button
+          className="mypage-edit-button"
+          onClick={() => toggleEdit("basic")}
+        >
+          {editableSections.basic ? "완료" : "수정"}
+        </button>
       </div>
       <hr />
       <div className="mypage-phone">
         <span className="mypage-label">전화번호</span>
-        <input type="tel" value={user.phone} />
-        <button className="mypage-edit-button">수정</button>
+        <input
+          type="tel"
+          value={user.phone}
+          disabled={!editableSections.phone}
+          onChange={(e) => handleChange("phone", e.target.value)}
+        />
+        <button
+          className="mypage-edit-button"
+          onClick={() => toggleEdit("phone")}
+        >
+          {editableSections.phone ? "완료" : "수정"}
+        </button>
       </div>
       <hr />
       <div className="mypage-email">
         <span className="mypage-label">이메일</span>
-        <input type="email" value={user.email} />
-        <button className="mypage-edit-button">수정</button>
+        <input
+          type="email"
+          value={user.email}
+          disabled={!editableSections.email}
+          onChange={(e) => handleChange("email", e.target.value)}
+        />
+        <button
+          className="mypage-edit-button"
+          onClick={() => toggleEdit("email")}
+        >
+          {editableSections.email ? "완료" : "수정"}
+        </button>
       </div>
       <hr />
       <div className="mypage-password">
         <span className="mypage-label">비밀번호</span>
-        <input type="password" value={user.password} />
-        <button className="mypage-edit-button">수정</button>
+        <div className="mypage-password-input-wrapper">
+          <input
+            type={showPassword ? "text" : "password"}
+            value={user.password}
+            disabled={!editableSections.password}
+            onChange={(e) => handleChange("password", e.target.value)}
+          />
+          <button
+            type="button"
+            className="mypage-view-button"
+            onMouseEnter={() => setShowPassword(true)}
+            onMouseLeave={() => setShowPassword(false)}
+            onFocus={() => setShowPassword(true)}
+            onBlur={() => setShowPassword(false)}
+          >
+            {showPassword ? <FaEyeSlash /> : <FaEye />}
+          </button>
+        </div>
+        <button
+          className="mypage-edit-button"
+          onClick={() => toggleEdit("password")}
+        >
+          {editableSections.password ? "완료" : "수정"}
+        </button>
       </div>
     </div>
   );
