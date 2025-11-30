4baf26116299638bda5e7f0d7a23bed067c788da
l2juhan <leejuhan0527@gmail.com>
2025-11-30 16:25:24 +0900

    Header.jsx에 알람 버튼 추가

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { MdNotificationsNone } from "react-icons/md";
 import "../../styles/header.css";
 import logoImage from "../../image/logo.png";
 
@@ -11,6 +12,9 @@ export default function Header() {
     <header className="header-bar">
       <img src={logoImage} alt="월급 관리소" className="header-logo" />
       <div>
+        <button className="header-icon" aria-label="알림">
+          <MdNotificationsNone />
+        </button>
         <span>{user.name} 님</span>
         <a href="#">로그아웃</a>
       </div>
