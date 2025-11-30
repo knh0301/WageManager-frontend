8ddf3ef1c6d03abf82897bdb68c1e249b0dc2567
l2juhan <leejuhan0527@gmail.com>
2025-12-01 02:42:36 +0900

    authApi.js 디버그용 console.log제거

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -2,8 +2,6 @@ import httpClient from './httpClient';
 
 // 카카오 액세스 토큰으로 로그인
 export const kakaoLoginWithToken = async (kakaoAccessToken) => {
-  console.log('kakaoLoginWithToken 호출, 받은 토큰:', kakaoAccessToken);
-  console.log('요청 Body에 보낼 데이터:', { kakaoAccessToken });
   // 카카오 로그인 API는 인증이 필요 없으므로 Authorization 헤더 제거
   return httpClient.post('/api/auth/kakao/login', { kakaoAccessToken }, {
     headers: {
